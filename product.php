77d0e5ecb8a2dc1dbdb11a9d8c8de96b012de24f
JohnnyHellstrom <124794710+JohnnyHellstrom@users.noreply.github.com>
2023-02-24 00:20:24 +0100

    verkligen desgin om DB..

--- product.php
+++ product.php
@@ -2,7 +2,6 @@
 session_start();
 include('view/header.php');
 
-//var_dump($_SESSION['cart']);
 // unset($_SESSION);
 // session_destroy();
 
@@ -15,11 +14,6 @@ if(isset($_SESSION["cart"])){
 } else {
    $_SESSION["cart"] = array($_SESSION["product"] => (int)$_POST['quantity']);
 }
-
-
-
-
-
 
 
 // echo '<pre>';
