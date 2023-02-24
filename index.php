3b3227a0ece69cde3b4dbbf228d7a4c01ac143ed
peterstromstedt <110103285+peterstromstedt@users.noreply.github.com>
2023-02-24 15:02:32 +0100

    så index ser lite bättre ut

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 include('view/header.php');
 ?>
-
+<link rel="stylesheet" href="css/header.css">
+<link rel="stylesheet" href="css/main.css">
 <a href="view/viewAllProducts.php"><button>view all products</button></a>
 <a href="admin/adminview/adminLoginForm.php"><button>login / signup</button></a>
 
