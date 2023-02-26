28781dd93e48059664c70fbb37cf14f8bc7f9f3e
peterstromstedt <110103285+peterstromstedt@users.noreply.github.com>
2023-02-26 23:05:30 +0100

    email sending done, just need to be styled

--- customerinfo.php
+++ customerinfo.php
@@ -1,5 +1,39 @@
 <?php
 require_once('classes/classDBClasses.php');
+require('vendor/autoload.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+// creating an instance of the phpmailer class passing in true as the first argument
+// this argument will configure phpmailer to throw an exception if there is a problem
+$mail = new PHPMailer(true);
+
+// we are telling the mail that we are using an smpt server by calling the smpt method
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = 'sinusskateshop@gmail.com';
+$mail->Password = 'tllgronvvofsgrlv';
+
+$storeEmail = 'sinusskateshop@gmail.com';
+$storeConfirmation = 'Sinus Skate Shop';
+
+
+// $mail->setFrom($email, $name);
+// $mail->addAddress('pstromstedt@telia.com');
+
+
+// $mail->Subject = $lastname;
+// $mail->Body = $country;
+
+
+
+
 session_start();
 
 
@@ -29,11 +63,20 @@ switch($action)
     // getting customer id
     $customerId = Customer::retrieveCustomerId($_POST['email']);  
     // update order and order details
+    $mail->setFrom($storeEmail, $storeConfirmation);
+    $mail->addAddress($_POST['email']);
+
+
+    // $mail->isHTML(true);
+    $mail->Subject = $lastname;
+    $mail->Body = $country;
+    $mail->send();
+    echo 'thank you ordering, a confirmation email has been sent!';
     break;
 
   case 'newcustomerinfo':
     $newCustomer = new Customer(ucfirst($firstname), ucfirst($lastname), strtolower($email), $phone, ucfirst($streetadress), $zipcode, ucfirst($city), ucfirst($country));
-    $lastId = $newCustomer->insertInfoToDB();
+    $lastId = $newCustomer->insertInfoToDB();   
     break;
   
   case 'existingcustomer':   
@@ -41,3 +84,10 @@ switch($action)
     header('location: view/viewExistingCustomerform.php');   
     break;
 }
+
+// https://github.com/phpmailer/phpmailer
+// download composer to be able to install the mail server thingie in cmd prompt
+// using the composer thingie from that github
+
+// sinusskateshop@gmail.com
+// pw: b@llong2023
