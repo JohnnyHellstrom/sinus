8da4d8c391bfff8d3fe21c36dce5e76ce01054c9
Sheldozer <125883340+Sheldozer@users.noreply.github.com>
2023-02-28 15:14:19 +0100

    lite css + redirect efter add to cart

--- product.php
+++ product.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 
-
-// unset($_SESSION);
-// session_destroy();
-
 if(isset($_SESSION["cart"])){
    if(array_key_exists($_SESSION["product"], $_SESSION["cart"])){
       $_SESSION["cart"][$_SESSION["product"]] += (int)$_POST['quantity'];
@@ -15,13 +11,5 @@ if(isset($_SESSION["cart"])){
    $_SESSION["cart"] = array($_SESSION["product"] => (int)$_POST['quantity']);
 }
 
-include('view/header.php');
-// echo '<pre>';
-// var_dump($_SESSION["cart"]);
-// echo '</pre>';
+header("location:index.php");
 ?>
-<a href="Cart.php">Cart</a>
-<a href=".">All products</a>
-
-<?php 
-include('view/footer.php');
